4181a47df75326ed1f7c37922a2f02dff04c42eb
AldiTorralvo <aldana.recruiterit@gmail.com>
2020-10-30 10:05:18 -0300

    my first homework

---
+++ 02-JS-I/homework/tests/Practica1.js
@@ -0,0 +1,41 @@
+var firstName = 'Aldi';
+let lastName = 'Torralvo';
+const favoritband = 'Pearl Jam';
+
+console.log ( firstName);
+console.log ( lastName);
+console.log ( favoriteband);
+
+
+// STRINGS
+var cat = 'Capital';
+
+// NUMBERS
+
+var num = 4;
+var real = 4.5; 
+var neg = -4;
+
+//BOOLEAN 
+
+var meGusta = true;
+var estoyAburrido = false;
+
+// NULL Y UNDEFINED
+
+var age 
+age 
+
+var age = null;
+
+//FUNCIONES: ESTRUCTURA DE UNA FUNCION 
+
+function myFunction() {
+	//CODIGO
+}
+const otherFunction = function() {
+	//CODIGO
+}
+const other = () => {
+	//CODIGO
+}
